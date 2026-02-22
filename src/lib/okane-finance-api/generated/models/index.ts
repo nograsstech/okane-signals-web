ba48980da8fdde37cbcd98fafeea1151ac2aa06e
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2026-02-23 01:59:20 +0700

    feat: add okane-finance-api openapi generator

---
+++ src/lib/okane-finance-api/generated/models/index.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AlphaVantageNewsResponseDTO';
+export * from './BacktestProcessUuid';
+export * from './BacktestResponseDTO';
+export * from './BacktestStats';
+export * from './End';
+export * from './HTTPValidationError';
+export * from './Limit';
+export * from './MessageRequest';
+export * from './Parameters';
+export * from './Period';
+export * from './RootResponse';
+export * from './Signal';
+export * from './SignalRequestData';
+export * from './SignalResponseDTO';
+export * from './SignalsDict';
+export * from './Sort';
+export * from './Start';
+export * from './Strategy';
+export * from './StrategyId';
+export * from './TickerResponseDTO';
+export * from './Tickers';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
